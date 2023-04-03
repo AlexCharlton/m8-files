fd43135927e22c38d71625c71696c223a346432a
Alex Charlton <alex.n.charlton@gmail.com>
2023-04-02 17:51:27 -0700

    Add Mod support

--- src/lib.rs
+++ src/lib.rs
@@ -1194,7 +1194,7 @@ impl Instrument {
                 let mult = reader.read();
                 let warp = reader.read();
                 let mirror = reader.read();
-                let synth_params = SynthParams::from_reader(reader, volume, pitch, fine_tune)?;
+                let synth_params = SynthParams::from_reader2(reader, volume, pitch, fine_tune)?;
                 finalize();
                 Self::WavSynth(WavSynth {
                     number,
@@ -1217,7 +1217,7 @@ impl Instrument {
                 let color = reader.read();
                 let degrade = reader.read();
                 let redux = reader.read();
-                let synth_params = SynthParams::from_reader(reader, volume, pitch, fine_tune)?;
+                let synth_params = SynthParams::from_reader2(reader, volume, pitch, fine_tune)?;
                 finalize();
                 Self::MacroSynth(MacroSynth {
                     number,
@@ -1241,7 +1241,7 @@ impl Instrument {
                 let loop_start = reader.read();
                 let length = reader.read();
                 let degrade = reader.read();
-                let synth_params = SynthParams::from_reader(reader, volume, pitch, fine_tune)?;
+                let synth_params = SynthParams::from_reader2(reader, volume, pitch, fine_tune)?;
                 reader.set_pos(start_pos + 0x57);
                 let sample_path = reader.read_string(128);
                 finalize();
@@ -1269,6 +1269,7 @@ impl Instrument {
                 let program_change = reader.read();
                 reader.read_bytes(3); // discard
                 let custom_cc: [ControlChange; 8] = arr![ControlChange::from_reader(reader)?; 8];
+                let mods = arr![AHDEnv::default().to_mod(); 4];
                 finalize();
                 Self::MIDIOut(MIDIOut {
                     number,
@@ -1281,6 +1282,7 @@ impl Instrument {
                     bank_select,
                     program_change,
                     custom_cc,
+                    mods,
                 })
             }
             0x04 => {
@@ -1310,7 +1312,7 @@ impl Instrument {
                 let mod2 = reader.read();
                 let mod3 = reader.read();
                 let mod4 = reader.read();
-                let synth_params = SynthParams::from_reader(reader, volume, pitch, fine_tune)?;
+                let synth_params = SynthParams::from_reader2(reader, volume, pitch, fine_tune)?;
                 finalize();
 
                 Self::FMSynth(FMSynth {
@@ -1338,7 +1340,9 @@ impl Instrument {
 
     fn from_reader3(reader: &Reader, number: u8, version: Version) -> Result<Self> {
         let start_pos = reader.pos();
+        println!("start pos: {:02x} ({})", start_pos, start_pos);
         let kind = reader.read();
+        dbg!(kind);
         let name = reader.read_string(12);
         let transpose = reader.read_bool();
         let table_tick = reader.read();
@@ -1359,7 +1363,13 @@ impl Instrument {
                 let mult = reader.read();
                 let warp = reader.read();
                 let mirror = reader.read();
-                let synth_params = SynthParams::from_reader(reader, volume, pitch, fine_tune)?;
+                println!("synth params pos: {:02x} ({})", reader.pos(), reader.pos());
+                let synth_params = SynthParams::from_reader3(reader, volume, pitch, fine_tune, 30)?;
+                println!(
+                    "synth params end pos: {:02x} ({})",
+                    reader.pos(),
+                    reader.pos()
+                );
                 finalize();
                 Self::WavSynth(WavSynth {
                     number,
@@ -1381,7 +1391,7 @@ impl Instrument {
                 let color = reader.read();
                 let degrade = reader.read();
                 let redux = reader.read();
-                let synth_params = SynthParams::from_reader(reader, volume, pitch, fine_tune)?;
+                let synth_params = SynthParams::from_reader3(reader, volume, pitch, fine_tune, 30)?;
                 finalize();
                 Self::MacroSynth(MacroSynth {
                     number,
@@ -1404,7 +1414,7 @@ impl Instrument {
                 let loop_start = reader.read();
                 let length = reader.read();
                 let degrade = reader.read();
-                let synth_params = SynthParams::from_reader(reader, volume, pitch, fine_tune)?;
+                let synth_params = SynthParams::from_reader3(reader, volume, pitch, fine_tune, 29)?;
                 reader.set_pos(start_pos + 0x57);
                 let sample_path = reader.read_string(128);
                 finalize();
@@ -1431,6 +1441,8 @@ impl Instrument {
                 let program_change = reader.read();
                 reader.read_bytes(3); // discard
                 let custom_cc: [ControlChange; 8] = arr![ControlChange::from_reader(reader)?; 8];
+                let _discard = reader.read_bytes(25);
+                let mods = arr![Mod::from_reader(reader)?; 4];
                 finalize();
                 Self::MIDIOut(MIDIOut {
                     number,
@@ -1443,6 +1455,7 @@ impl Instrument {
                     bank_select,
                     program_change,
                     custom_cc,
+                    mods,
                 })
             }
             0x04 => {
@@ -1471,7 +1484,7 @@ impl Instrument {
                 let mod2 = reader.read();
                 let mod3 = reader.read();
                 let mod4 = reader.read();
-                let synth_params = SynthParams::from_reader(reader, volume, pitch, fine_tune)?;
+                let synth_params = SynthParams::from_reader3(reader, volume, pitch, fine_tune, 2)?;
                 finalize();
 
                 Self::FMSynth(FMSynth {
@@ -1583,6 +1596,8 @@ pub struct MIDIOut {
     pub bank_select: u8,
     pub program_change: u8,
     pub custom_cc: [ControlChange; 8],
+
+    pub mods: [Mod; 4],
 }
 
 #[derive(PartialEq, Debug, Clone)]
@@ -1604,11 +1619,10 @@ pub struct SynthParams {
     pub mixer_delay: u8,
     pub mixer_reverb: u8,
 
-    pub envelopes: [Envelope; 2],
-    pub lfos: [LFO; 2],
+    pub mods: [Mod; 4],
 }
 impl SynthParams {
-    fn from_reader(reader: &Reader, volume: u8, pitch: u8, fine_tune: u8) -> Result<Self> {
+    fn from_reader2(reader: &Reader, volume: u8, pitch: u8, fine_tune: u8) -> Result<Self> {
         Ok(Self {
             volume,
             pitch,
@@ -1627,31 +1641,127 @@ impl SynthParams {
             mixer_delay: reader.read(),
             mixer_reverb: reader.read(),
 
-            envelopes: arr![Envelope::from_reader(reader)?; 2],
-            lfos: arr![LFO::from_reader(reader)?; 2],
+            mods: [
+                AHDEnv::from_reader2(reader)?.to_mod(),
+                AHDEnv::from_reader2(reader)?.to_mod(),
+                LFO::from_reader2(reader)?.to_mod(),
+                LFO::from_reader2(reader)?.to_mod(),
+            ],
+        })
+    }
+
+    fn from_reader3(
+        reader: &Reader,
+        volume: u8,
+        pitch: u8,
+        fine_tune: u8,
+        mod_offset: usize,
+    ) -> Result<Self> {
+        let filter_type = reader.read();
+        let filter_cutoff = reader.read();
+        let filter_res = reader.read();
+
+        let amp = reader.read();
+        let limit = reader.read();
+
+        let mixer_pan = reader.read();
+        let mixer_dry = reader.read();
+        let mixer_chorus = reader.read();
+        let mixer_delay = reader.read();
+        let mixer_reverb = reader.read();
+        let _discard = reader.read_bytes(mod_offset);
+        let mods = arr![Mod::from_reader(reader)?; 4];
+
+        Ok(Self {
+            volume,
+            pitch,
+            fine_tune,
+
+            filter_type,
+            filter_cutoff,
+            filter_res,
+
+            amp,
+            limit,
+
+            mixer_pan,
+            mixer_dry,
+            mixer_chorus,
+            mixer_delay,
+            mixer_reverb,
+
+            mods,
         })
     }
 }
 
 #[derive(PartialEq, Debug, Clone)]
-pub struct Envelope {
+pub enum Mod {
+    AHDEnv(AHDEnv),
+    ADSREnv(ADSREnv),
+    DrumEnv(DrumEnv),
+    LFO(LFO),
+    TrigEnv(TrigEnv),
+    TrackingEnv(TrackingEnv),
+}
+
+impl Mod {
+    const SIZE: usize = 6;
+    fn from_reader(reader: &Reader) -> Result<Self> {
+        let start_pos = reader.pos();
+        let first_byte = reader.read();
+        let ty = first_byte >> 4;
+        let dest = first_byte & 0x0F;
+
+        dbg!(ty, dest, start_pos);
+        let r = match ty {
+            0 => Mod::AHDEnv(AHDEnv::from_reader3(reader, dest)?),
+            1 => Mod::ADSREnv(ADSREnv::from_reader(reader, dest)?),
+            2 => Mod::DrumEnv(DrumEnv::from_reader(reader, dest)?),
+            3 => Mod::LFO(LFO::from_reader3(reader, dest)?),
+            4 => Mod::TrigEnv(TrigEnv::from_reader(reader, dest)?),
+            5 => Mod::TrackingEnv(TrackingEnv::from_reader(reader, dest)?),
+            x => panic!("Unknown mod type {}", x),
+        };
+
+        reader.set_pos(start_pos + Self::SIZE);
+        Ok(r)
+    }
+}
+
+#[derive(PartialEq, Debug, Clone, Default)]
+pub struct AHDEnv {
     pub dest: u8,
     pub amount: u8,
     pub attack: u8,
     pub hold: u8,
     pub decay: u8,
-    pub retrigger: u8,
 }
-impl Envelope {
-    fn from_reader(reader: &Reader) -> Result<Self> {
-        Ok(Self {
+impl AHDEnv {
+    fn from_reader2(reader: &Reader) -> Result<Self> {
+        let r = Self {
             dest: reader.read(),
             amount: reader.read(),
             attack: reader.read(),
             hold: reader.read(),
             decay: reader.read(),
-            retrigger: reader.read(),
+        };
+        reader.read();
+        Ok(r)
+    }
+
+    fn from_reader3(reader: &Reader, dest: u8) -> Result<Self> {
+        Ok(Self {
+            dest,
+            amount: reader.read(),
+            attack: reader.read(),
+            hold: reader.read(),
+            decay: reader.read(),
         })
+    }
+
+    fn to_mod(self) -> Mod {
+        Mod::AHDEnv(self)
     }
 }
 
@@ -1662,17 +1772,115 @@ pub struct LFO {
     pub trigger_mode: u8,
     pub freq: u8,
     pub amount: u8,
-    pub retrigger: u8,
 }
 impl LFO {
-    fn from_reader(reader: &Reader) -> Result<Self> {
-        Ok(Self {
+    fn from_reader2(reader: &Reader) -> Result<Self> {
+        let r = Self {
             shape: reader.read(),
             dest: reader.read(),
             trigger_mode: reader.read(),
             freq: reader.read(),
             amount: reader.read(),
-            retrigger: reader.read(),
+        };
+        reader.read();
+        Ok(r)
+    }
+
+    fn from_reader3(reader: &Reader, dest: u8) -> Result<Self> {
+        Ok(Self {
+            dest,
+            amount: reader.read(),
+            shape: reader.read(),
+            trigger_mode: reader.read(),
+            freq: reader.read(),
+        })
+    }
+
+    fn to_mod(self) -> Mod {
+        Mod::LFO(self)
+    }
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct ADSREnv {
+    pub dest: u8,
+    pub amount: u8,
+    pub attack: u8,
+    pub decay: u8,
+    pub sustain: u8,
+    pub release: u8,
+}
+impl ADSREnv {
+    fn from_reader(reader: &Reader, dest: u8) -> Result<Self> {
+        Ok(Self {
+            dest,
+            amount: reader.read(),
+            attack: reader.read(),
+            decay: reader.read(),
+            sustain: reader.read(),
+            release: reader.read(),
+        })
+    }
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct DrumEnv {
+    pub dest: u8,
+    pub amount: u8,
+    pub peak: u8,
+    pub body: u8,
+    pub decay: u8,
+}
+impl DrumEnv {
+    fn from_reader(reader: &Reader, dest: u8) -> Result<Self> {
+        Ok(Self {
+            dest,
+            amount: reader.read(),
+            peak: reader.read(),
+            body: reader.read(),
+            decay: reader.read(),
+        })
+    }
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct TrigEnv {
+    pub dest: u8,
+    pub amount: u8,
+    pub attack: u8,
+    pub hold: u8,
+    pub decay: u8,
+    pub src: u8,
+}
+impl TrigEnv {
+    fn from_reader(reader: &Reader, dest: u8) -> Result<Self> {
+        Ok(Self {
+            dest,
+            amount: reader.read(),
+            attack: reader.read(),
+            hold: reader.read(),
+            decay: reader.read(),
+            src: reader.read(),
+        })
+    }
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct TrackingEnv {
+    pub dest: u8,
+    pub amount: u8,
+    pub src: u8,
+    pub lval: u8,
+    pub hval: u8,
+}
+impl TrackingEnv {
+    fn from_reader(reader: &Reader, dest: u8) -> Result<Self> {
+        Ok(Self {
+            dest,
+            amount: reader.read(),
+            src: reader.read(),
+            lval: reader.read(),
+            hval: reader.read(),
         })
     }
 }
